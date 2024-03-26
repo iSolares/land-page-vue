b031e330fcdd71adc8586948dce17ed55dcfb4b3
Moisés Solares Santos <moisessolaressantos@gmail.com>
2024-03-26 17:31:49 -0300

    Add Map and render page until maps

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,68 +1,190 @@
 <template>
-    <div class="h-100 w-100">
-        <v-img class="w-100 position-absolute" src="../../assets/img/iniciodapagina.png"/>
-            <v-img class="w-100 size-init-title" src="../../assets/img/iniciodapaginatitulo.png"/>
-      <v-row>
-          <v-col cols="12">
+  <div class="h-100 w-100">
+    <v-row>
+      <v-col cols="12">
+        <v-img
+          eager
+          class="w-100 position-absolute"
+          src="../../assets/img/iniciodapagina.png"
+        />
+        <v-img
+          eager
+          class="w-100 size-init-title"
+          src="../../assets/img/iniciodapaginatitulo.png"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
         <v-carousel
-          height="500"
+          height="100%"
           show-arrows="hover"
           cycle
           interval="3000"
-          hide-delimiter-background
+          hide-delimiters
         >
-          <v-carousel-item sizes="1000" src="../../assets/img/carrossel1.png">
-          </v-carousel-item>
-          <v-carousel-item src="../../assets/img/carrossel2.png">
-          </v-carousel-item>
-          <v-carousel-item src="../../assets/img/carrossel3.png">
-          </v-carousel-item>
-          <v-carousel-item src="../../assets/img/carrossel4.png">
-          </v-carousel-item>
-          <v-carousel-item src="../../assets/img/carrossel5.png">
-          </v-carousel-item>
-          <v-carousel-item src="../../assets/img/carrossel6.png">
-          </v-carousel-item>
-          <v-carousel-item src="../../assets/img/carrossel7.png">
-          </v-carousel-item>
+          <v-carousel-item
+            eager
+            src="../../assets/img/carrossel1.png"
+          ></v-carousel-item>
+          <v-carousel-item
+            eager
+            src="../../assets/img/carrossel2.png"
+          ></v-carousel-item>
+          <v-carousel-item
+            eager
+            src="../../assets/img/carrossel3.png"
+          ></v-carousel-item>
+          <v-carousel-item
+            eager
+            src="../../assets/img/carrossel4.png"
+          ></v-carousel-item>
+          <v-carousel-item
+            eager
+            src="../../assets/img/carrossel5.png"
+          ></v-carousel-item>
+          <v-carousel-item
+            eager
+            src="../../assets/img/carrossel6.png"
+          ></v-carousel-item>
+          <v-carousel-item
+            eager
+            src="../../assets/img/carrossel7.png"
+          ></v-carousel-item>
         </v-carousel>
       </v-col>
     </v-row>
-</div>
-
+    <v-row>
+      <v-img
+        eager
+        class="w-100"
+        src="../../assets/img/horariodefuncionamentofundo.png"
+      >
+        <v-row>
+          <v-col cols="5">
+            <v-img
+              cover
+              eager
+              height="1600"
+              src="../../assets/img/coringahorario.png"
+            >
+            </v-img>
+          </v-col>
+          <v-col class="flex-column" cols="7">
+            <v-img eager src="../../assets/img/horariotitulo.png" />
+            <v-img
+              eager
+              class="mr-16"
+              src="../../assets/img/horáriosedias.png"
+            />
+            <v-img
+              eager
+              class="justify-end"
+              src="../../assets/img/duvidastitulo.png"
+            />
+            <v-img
+              eager
+              class="justify-end"
+              src="../../assets/img/duvidas.png"
+            />
+          </v-col>
+        </v-row>
+      </v-img>
+    </v-row>
+    <v-row>
+      <v-col class="pa-0" cols="2">
+        <v-img
+          height="700"
+          width="220"
+          class="position-arrow-left"
+          src="../../assets/img/cantoporquereservar.png"
+        />
+      </v-col>
+      <v-col class="pa-0" cols="8">
+        <v-img src="../../assets/img/reservatitulo.png" />
+        <v-img src="../../assets/img/precisareservar_texto.png" />
+      </v-col>
+      <v-col class="pa-0" cols="2">
+        <v-img
+          height="700"
+          width="220"
+          class="position-arrow-right"
+          src="../../assets/img/precisareservarcanto.png"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <img width="100%" src="../../assets/img/sobrenostitulo.png" />
+      </v-col>
+      <v-col cols="12">
+        <v-img src="../../assets/img/sobreajoksquadros.png" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-img src="../../assets/img/botaoporquevir.png" />
+      </v-col>
+      <v-col cols="12">
+        <v-img src="../../assets/img/motivos.png" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-img src="../../assets/img/feedback.png" />
+      </v-col>
+      <v-col cols="12">
+        <v-img src="../../assets/img/feedbackcliente1.png" />
+      </v-col>
+      <v-col cols="12">
+        <v-img src="../../assets/img/feedbackcliente3.png" />
+      </v-col>
+      <v-col cols="12">
+        <v-img src="../../assets/img/feedbackcliente2.png" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-img src="../../assets/img/localizaçãobotao.png" />
+      </v-col>
+      <v-col class="text-center" cols="12">
+        <div id="map">
+          <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+            <l-tile-layer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              layer-type="base"
+              name="OpenStreetMap"
+            ></l-tile-layer>
+          </l-map>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script setup lang="ts">
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 import { ref } from "vue";
-const items = ref([
-    {
-        src:'../../assets/img/carrossel1.png'
-    },
-    {
-        src:"../../assets/img/carrossel2.png"
-    },
-    {
-        src:"../../assets/img/carrossel3.png"
-    },
-    {
-        src:"../../assets/img/carrossel4.png"
-    },
-    {
-        src:"../../assets/img/carrossel5.png"
-    },
-    {
-        src:"../../assets/img/carrossel6.png"
-    },
-    {
-        src:"../../assets/img/carrossel7.png"
-    },
-   
-]);
 
+const zoom = ref(2);
 </script>
 <style scoped>
+#map {
+  width: 700px;
+  height: 400px;
+}
 .size-init-title {
-    padding-top: 500px ;
+  padding-top: 1100px;
 }
 
+.position-arrow-left {
+  margin-top: 1px;
+  margin-left: 12px;
+}
+
+.position-arrow-right {
+  margin-left: 25px;
+}
 </style>
